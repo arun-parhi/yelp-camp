34f2399a36e5b6ace367f8fd4ce5cd8a24ed4062
Arun <arun.kumar@rapidfunnel.com>
2023-02-21 09:04:27 +0530

    Get port from env

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
     res.send("Hello from Yelp Camp !!");
 })
 
-app.listen(3000, () => { 
+const port = process.env.PORT || 3000;
+app.listen(port, () => { 
     console.log("Serving on port 3000");
 })
