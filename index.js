cbaa6093f5318026c8ab576311e5b683312db6c5
Arun <arun.kumar@rapidfunnel.com>
2023-02-21 13:50:48 +0530

    Integration of views and momdels

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 
+const path = require("path");
+const Campground = require("./models/campground");
+
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser : true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
 const app = express();
 
-app.get('/', (req, res) => { 
-    res.send("Hello from Yelp Camp !!");
-})
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({
+        title: 'My Campground',
+        description : 'Cheap campaign'
+    });
+    await camp.save();
+    res.send(camp);
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => { 
+app.listen(port, () => {
     console.log("Serving on port 3000");
-})
+});
